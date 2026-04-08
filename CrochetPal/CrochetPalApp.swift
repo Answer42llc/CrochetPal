1ba43c44b99d0037325199af855b02a0d57236bc
ricky0603 <wang.liao1@gmail.com>
2026-04-08 15:55:58 +0800

    Initial Commit

---
+++ CrochetPal/CrochetPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrochetPalApp.swift
+//  CrochetPal
+//
+//  Created by lw on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct CrochetPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
